5a06c2ce08b69ea131f8bc38d1ab68755ed6a3e3
longthanh207 <nguyenthanhlongpr4@gmail.com>
2026-03-28 17:44:51 +0700

    Fix duplicate cors declaration

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-app.use(cors()); // Cho phép mọi nguồn truy cập vào API
 const mongoose = require('mongoose');
 const cors = require('cors');
+app.use(cors()); // Cho phép mọi nguồn truy cập vào API
 const path = require('path');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
